e64878f3dadbe2ea1e4db9aff7672ece1def15e6
Benatovich <sambenatovich@gmail.com>
2022-01-14 17:26:07 -0500

    html main-page set up, files and folders added

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://bldkvwcsogzeeohrgemf.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MjExOTQxMiwiZXhwIjoxOTU3Njk1NDEyfQ.ZB2Np0Wtsn23P3JjRuzH5v11_nI4pm9CfkE63toNYLc';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
