cdf1617c4d9810494a6111ef7d54a48a3115114c
Benatovich <sambenatovich@gmail.com>
2022-01-14 20:32:58 -0500

    crossing our fingers and saying our prayers

--- render-utils.js
+++ render-utils.js
@@ -1,14 +1,22 @@
 export function renderProfile(profile) {
+// use an if/else statement to check if this is the user that's logged in; use that to determine how to render the profile
+// if it is the logged in user, try changing divs to inputs
     const profileEl = document.createElement('div');
     const talentsDiv = document.createElement('div');
     const nameEl = document.createElement('p');
+    const aboutEl = document.createElement('p');
+    const locationEl = document.createElement('p');
+    const interestsEl = document.createElement('p');
 
     profileEl.classList.add('profile');
     talentsDiv.classList.add('talents');
 
     nameEl.textContent = profile.name;
+    aboutEl.textContent = profile.about;
+    locationEl.textContent = profile.location;
+    interestsEl.textContent = profile.interests;
 
-    for (let talent of profiles.talents) {
+    for (let talent of profile.talents) {
         const haveEl = document.createElement('p');
         const wantEl = document.createElement('p');
 
@@ -17,7 +25,7 @@ export function renderProfile(profile) {
 
         talentsDiv.append(haveEl, wantEl);
     }
-    profileEl.append(nameEl, talentsDiv);
+    profileEl.append(nameEl, locationEl, talentsDiv, interestsEl, aboutEl);
     // profilesEl.append(profileEl);
 
     return profileEl;
