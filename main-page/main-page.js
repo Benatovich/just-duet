431efd1a8f166e458b16f170bb51d02f36cc4a50
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-18 10:55:14 -0800

    rewrote displayProfiles function

--- main-page/main-page.js
+++ main-page/main-page.js
@@ -23,21 +23,19 @@ async function displayProfiles() {
         const profileEl = document.createElement('div');
         const talentsDiv = document.createElement('div');
         const nameEl = document.createElement('p');
+        const haveEl = document.createElement('p');
+        const wantEl = document.createElement('p');
 
         profileEl.classList.add('profile');
         talentsDiv.classList.add('talents');
 
         nameEl.textContent = profile.name;
+        haveEl.textContent = profile.have_talents;
+        wantEl.textContent = profile.want_talents;
 
-        for (let talent of profiles.talents) {
-            const haveEl = document.createElement('p');
-            const wantEl = document.createElement('p');
-
-            haveEl.textContent = talent.talents.have_id;
-            wantEl.textContent = talent.talents.want_id;
-
-            talentsDiv.append(haveEl, wantEl);
-        }
+    
+        talentsDiv.append(haveEl, wantEl);
+        
         profileEl.append(nameEl, talentsDiv);
         profilesEl.append(profileEl);
     }
