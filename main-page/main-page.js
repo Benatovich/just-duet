31bbc47a921b54d0b3bf6ef4dd8c8aa1ba18fcb4
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-20 16:01:29 -0700

    addded search form

--- main-page/main-page.js
+++ main-page/main-page.js
@@ -2,7 +2,8 @@ import { checkAuth,
     logout, 
     fetchProfiles, 
     getUser, 
-    getUserId } from '../fetch-utils.js';
+    getUserId,
+    searchFunction } from '../fetch-utils.js';
 
 import { renderProfile } from '../render-utils.js';
 
@@ -11,13 +12,23 @@ checkAuth();
 const logoutButton = document.getElementById('logout');
 const profilesEl = document.querySelector('.profiles-container');
 const myPageButton = document.getElementById('my-page');
-
+const searchForm = document.querySelector('.search-form');
 // testing shit here
 // const user = await getUser();
 // const userId = user.user.id;
 
 // const messageId = await getUserId(userId);
 // console.log(messageId);
+
+searchForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData(searchForm);
+    const search = data.get('search');
+    const searchInput = search.value;
+
+    await searchFunction(searchInput);
+});
 
 window.addEventListener('load', async() => {
     await displayProfiles();
